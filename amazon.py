918c40d60ad4288ba635ccf3617fa56a226e138f
Sarthak Batra <sarthakbatra1993@gmail.com>
2018-07-07 16:14:36 -0400

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -2,7 +2,17 @@ from bs4 import BeautifulSoup
 import re
 import requests
 a = []
-url = "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=children+supplements"
+url_str = input("Enter the product category you want to search for: ")
+try:
+	str1, str2 = url_str.split()
+except ValueError:
+	str2 = "empty"
+
+if (str2 == "empty"):
+	url = "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=" + url_str
+else:
+	url = "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=" + str1 + "+" + str2
+
 # add header
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
 r = requests.get(url, headers=headers)
@@ -16,13 +26,27 @@ f.write(headers)
 
 #Regex if needed
 # a = re.compile((?<=data-asin))
+
+# Used this to beautify and inspect the html/xml data ----> http://jsbeautifier.org/
 containers = soup.findAll("li", {"class":"s-result-item s-result-card-for-container a-declarative celwidget "})
+# if empty, page is styled different, invoke second style query s-result-item s-result-card-for-container s-carded-grid celwidget  
+if (len(containers) == 0):
+	containers = soup.findAll("li", {"class":"s-result-item s-result-card-for-container s-carded-grid celwidget "})
+	print("Style 2")
+
+if (len(containers) == 0):
+	containers = soup.findAll("li", {"class":"s-result-item celwidget "})
+	print("Style 3")
+
 for container in containers:
 	asin = (container["data-asin"])
 
 	# Product Name
-	title_container = container.findAll("a", {"class":"a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal"})
-	name = title_container[0]["title"]
+	try:
+		title_container = container.findAll("a", {"class":"a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal"})
+		name = title_container[0]["title"]
+	except:
+		name = "N/A"
 
 	# Product Price
 	# span class="a-offscreen">$14.67
@@ -34,11 +58,13 @@ for container in containers:
 
 	#Number of reviews
 	num_review_container = container.findAll("a", {"class":"a-size-small a-link-normal a-text-normal"})
-	if (len(num_review_container) > 1):
-		num_reviews = num_review_container[1].text.strip()
-	else: 
-		num_reviews = num_review_container[0].text.strip()
-
+	try:
+		if (len(num_review_container) > 1):
+			num_reviews = num_review_container[1].text
+		else: 
+			num_reviews = num_review_container[0].text
+	except:
+		num_reviews = "0"
 	#
 	#try:
 	#	num_review2 = num_review_container[1].text.strip()

5d5230e501db0a55f9122b7df72f29cf747bc263
Sarthak Batra <sarthakbatra1993@gmail.com>
2018-08-08 13:24:00 -0400

    Improved Organization
    Improved project organization and made the README comprehensive.

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # AmazonScrapper
 
-![](AmazonScrapper.png)
+This project performs web-scraping on www.amazon.com with the Python package Beautiful Soup. The user can enter any product cateogry and a *'csv'* file with the Amazon ASIN codes, product name, price, and number of reviews is generated. 
+
+## How can this help you?
+The initial inspiration for this project was to get a dump of Amazon ASIN codes, or ID's for any product category. API requests on those ASIN codes can give a detailed analysis of the inventory. However, services usually rate-limit your requests or make you pay a decent fee. This can be a problem, as I found out when tracking kitchen-ware over a month to visualize fluctuations. Thus, I decided to make a minimal but effective version which doesn't need any API requests.  
+
+As the images below will indicate, this can also be invaluable to small and new business in trying to gauge competition. Additionaly, investors can use this to monitor an entire commodity space. 
+
+#### Product Analysis
+![images/Product Analysis](images/ProductAnalysis.png)
+
+#### Review Distribution
+![images/Review Distribution](images/ReviewDistribution.png)
+
+
+## Further improvements 
+- Make this project more dynamic and deploy it in a presentable manner. 
+- There can be better validation for text-parsing. Regex was experimented with but not used in this version.
+- Amazon also displays products on their webpage in several different arrangements. This means different HTML elements and CSS classes, leading to more edge-cases. I have heavily tested different product categories to account for this but a deep-dive may reveal a generic way.
+
+## Getting Started
+If you have little Python experience, you can find plenty of resources on the internet to learn how to execute a Python file, clone a Github project on your local machine, or use external packages like [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/). 
+
+ - *Environment*: Here's a simple guide to help you install both Python 2.7 and Python 3.6 on [Windows 10](https://datascience.com.co/how-to-install-python-2-7-and-3-6-in-windows-10-add-python-path-281e7eae62a)
+
+## Contributing
+
+Please shoot me an email at sarthakbatra1993@gmail.com with the title 'Contributing to AmazonScrapper' if you have suggestions on how to make it better or would want to know more about pull requests.
+
+## Authors
+
+* **Sarthak Batra** - *Initial work* - [My Github](https://github.com/sarthakbatragatech)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ amazonScrapper.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+a = []
+url_str = input("Enter the product category you want to search for: ")
+try:
+	str1, str2 = url_str.split()
+except ValueError:
+	str2 = "empty"
+
+if (str2 == "empty"):
+	url = "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=" + url_str
+else:
+	url = "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=" + str1 + "+" + str2
+
+# add header
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+r = requests.get(url, headers=headers)
+soup = BeautifulSoup(r.content, "html.parser")
+
+#Csv writing setup
+filename = "products.csv"
+f = open(filename, "w", encoding='utf-8')
+headers ="Asin, Name, Price, Number of Reviews\n"
+f.write(headers)
+
+#Regex if needed
+# a = re.compile((?<=data-asin))
+
+# Used this to beautify and inspect the html/xml data ----> http://jsbeautifier.org/
+containers = soup.findAll("li", {"class":"s-result-item s-result-card-for-container a-declarative celwidget "})
+# if empty, page is styled different, invoke second style query s-result-item s-result-card-for-container s-carded-grid celwidget  
+if (len(containers) == 0):
+	containers = soup.findAll("li", {"class":"s-result-item s-result-card-for-container s-carded-grid celwidget "})
+	print("Style 2")
+
+if (len(containers) == 0):
+	containers = soup.findAll("li", {"class":"s-result-item celwidget "})
+	print("Style 3")
+
+for container in containers:
+	asin = (container["data-asin"])
+
+	# Product Name
+	try:
+		title_container = container.findAll("a", {"class":"a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal"})
+		name = title_container[0]["title"]
+	except:
+		name = "N/A"
+
+	# Product Price
+	# span class="a-offscreen">$14.67
+	price_container = container.findAll("span", {"class":"a-offscreen"})
+	if (len(price_container) == 1):
+		price = price_container[0].text
+	else:
+		price = "N/A"
+
+	#Number of reviews
+	num_review_container = container.findAll("a", {"class":"a-size-small a-link-normal a-text-normal"})
+	try:
+		if (len(num_review_container) > 1):
+			num_reviews = num_review_container[1].text
+		else: 
+			num_reviews = num_review_container[0].text
+	except:
+		num_reviews = "0"
+	#
+	#try:
+	#	num_review2 = num_review_container[1].text.strip()
+	#	print(num_review2)
+	# except list index out of range:
+	#	num_review2 = 0;
+	#
+
+	f.write(asin + ',' + name.replace(",", "|") + ',' + price.replace("$", "") + "," + num_reviews.replace(",", "") + "\n") # + ',' + name.replace(",", "|") + ',' + price + "," + num_reviews + "\n")
+
+f.close()
